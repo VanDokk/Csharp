75ff5e435b8d4ac811f946b544cfbb219a4e7493
VanDokk <48243610+VanDokk@users.noreply.github.com>
2019-03-09 16:53:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        Console.WriteLine("Введите два вещественных числа: ");
+        double num1 = 0, num2 = 0;
+
+        bool flag = false;
+        do
+        {            
+            try
+            {
+                num1 = Convert.ToDouble(Console.ReadLine());
+                num2 = Convert.ToDouble(Console.ReadLine());
+                flag = true;
+            }
+            catch
+            {
+                Console.WriteLine("Введите корректное значение(Возможно поменяйте . на ,)");
+            }
+            finally
+            {
+                double sum = num1 + num2;
+                Console.Write($"{sum}");
+            }
+        } while (flag != true);
+
+
+        
+        }
+    }
+
